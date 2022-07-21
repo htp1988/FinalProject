104777889b2c9fe9e15cfb6e9cb2a1b38be2d5b6
htp1988 <phuc210788@gmail.com>
2022-07-21 17:57:29 -0400

    final

--- TkinterLayout.py
+++ TkinterLayout.py
@@ -48,19 +48,31 @@ class App(customtkinter.CTk):
         self.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
         self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
 
-        self.title = customtkinter.CTkLabel(master=self.frame_left, text="Car Inventory", text_font=App.TEXT)
+        self.title = customtkinter.CTkLabel(master=self.frame_left,
+                                            text="Car Inventory",
+                                            text_font=App.TEXT)
         self.title.grid(row=1, column=0, pady=10, padx=10)
 
-        self.home_button = customtkinter.CTkButton(master=self.frame_left, text="Home", text_font=App.TEXT)
+        self.home_button = customtkinter.CTkButton(master=self.frame_left,
+                                                   text="Home",
+                                                   text_font=App.TEXT)
         self.home_button.grid(row=2, column=0, pady=10, padx=20)
 
-        self.add_car_button = customtkinter.CTkButton(master=self.frame_left, text="Add Car", text_font=App.TEXT, command=self.add_car_test)
+        self.add_car_button = customtkinter.CTkButton(master=self.frame_left,
+                                                      text="Add Car",
+                                                      text_font=App.TEXT,
+                                                      command=self.add_car_test)
         self.add_car_button.grid(row=3, column=0, pady=10, padx=20)
 
-        self.search_button = customtkinter.CTkButton(master=self.frame_left, text="Search", text_font=App.TEXT, command=self.search_car_test)
+        self.search_button = customtkinter.CTkButton(master=self.frame_left,
+                                                     text="Search",
+                                                     text_font=App.TEXT,
+                                                     command=self.search_car_test)
         self.search_button.grid(row=4, column=0, pady=10, padx=20)
 
-        self.color_mode_title = customtkinter.CTkLabel(master=self.frame_left, text="Appearance Mode:", text_font=App.TEXT)
+        self.color_mode_title = customtkinter.CTkLabel(master=self.frame_left,
+                                                       text="Appearance Mode:",
+                                                       text_font=App.TEXT)
         self.color_mode_title.grid(row=9, column=0, pady=0, padx=20, sticky="w")
 
         self.color_mode_toggle = customtkinter.CTkOptionMenu(master=self.frame_left,
@@ -89,11 +101,6 @@ class App(customtkinter.CTk):
     def add_car_test(self):
         """ Creates frame for a form containing: Make, model, color, year."""
         def get_items():
-            print(f'Car({ctk_items[1][0].get()}, {ctk_items[1][1].get()}, {ctk_items[1][2].get()}, {ctk_items[1][3].get()})')
-            newCar = UsedCar(random.randint(1, 50), ctk_items[1][0].get(), ctk_items[1][1].get(), ctk_items[1][2].get(), ctk_items[1][3].get())
-            newCar.save_to_file()
-
-        def successfully_add():
             newCar = UsedCar(random.randint(1, 50),
                              ctk_items[1][0].get(),
                              ctk_items[1][1].get(),
@@ -106,7 +113,7 @@ class App(customtkinter.CTk):
                                )
             if confirm:
                 res = customtkinter.CTkLabel(root, text="Successfully Added!!!")
-                res.grid(row=5, column=0, columnspan=2)
+                res.grid(row=10, column=0, columnspan=2)
                 newCar.save_to_file()
             else:
                 pass
@@ -118,19 +125,26 @@ class App(customtkinter.CTk):
         self.add_car_frame.grid(row=0, column=1, sticky="nswe", padx=30, pady=30)       # Full Frame == (master=self) and column=1
 
         for element in range(4):
-            ctk_items[0].append(customtkinter.CTkLabel(master=self.add_car_frame, text=f'{items[element]}:', text_font=App.TEXT))
+            ctk_items[0].append(customtkinter.CTkLabel(master=self.add_car_frame,
+                                                       text=f'{items[element]}:',
+                                                       text_font=App.TEXT))
             ctk_items[0][element].grid(row=element, column=0, padx=0, pady=15)
 
             ctk_items[1].append(customtkinter.CTkEntry(master=self.add_car_frame, text_font=App.TEXT))
             ctk_items[1][element].grid(row=element, column=1, padx=0, pady=15)
 
-        self.output_car_button = customtkinter.CTkButton(master=self.add_car_frame, text="Add Car", command=successfully_add)
+        self.output_car_button = customtkinter.CTkButton(master=self.add_car_frame,
+                                                         text="Add Car",
+                                                         command=get_items)
         self.output_car_button.grid(row=4, column=1, pady=15, padx=0)
 
     def search_car_test(self):
 
         def find_car():
-            newSearch = Inventory(ctk_items[1][0].get(), ctk_items[1][1].get(), ctk_items[1][2].get(), ctk_items[1][3].get())
+            newSearch = Inventory(ctk_items[1][0].get(),
+                                  ctk_items[1][1].get(),
+                                  ctk_items[1][2].get(),
+                                  ctk_items[1][3].get())
             returnedSearch = newSearch.search()
 
             ctk_items[1][0].delete(0, customtkinter.END)
@@ -148,13 +162,17 @@ class App(customtkinter.CTk):
         self.add_car_frame.grid(row=0, column=1, sticky="nswe", padx=30, pady=30)       # Full Frame == (master=self) and column=1
 
         for element in range(len(items)):
-            ctk_items[0].append(customtkinter.CTkLabel(master=self.add_car_frame, text=f'{items[element]}:', text_font=App.TEXT))
+            ctk_items[0].append(customtkinter.CTkLabel(master=self.add_car_frame,
+                                                       text=f'{items[element]}:',
+                                                       text_font=App.TEXT))
             ctk_items[0][element].grid(row=element, column=0, padx=0, pady=15)
 
             ctk_items[1].append(customtkinter.CTkEntry(master=self.add_car_frame, text_font=App.TEXT))
             ctk_items[1][element].grid(row=element, column=1, padx=0, pady=15)
 
-        self.search_car_button = customtkinter.CTkButton(master=self.add_car_frame, text="Search Inventory", command=find_car)
+        self.search_car_button = customtkinter.CTkButton(master=self.add_car_frame,
+                                                         text="Search Inventory",
+                                                         command=find_car)
         self.search_car_button.grid(row=4, column=1, pady=15, padx=0)
 
 
